6ff0e96e250f1d81958e092d392601daed44e3e3
RefatAli <mohamedrefat8434@gmail.com>
2023-07-11 15:14:10 +0300

    push to add

--- js/singleSlider.js
+++ js/singleSlider.js
@@ -4,14 +4,31 @@ const galleryConteiner = document.querySelector(".galleryConteiner");
 const buttonCloseGallery = document.querySelector(".buttonCloseGallery");
 const itineraryPackage = document.querySelectorAll(".itineraryPackage");
 const listItinerary = document.querySelectorAll(".itineraryList");
+const galleryImage = document.querySelectorAll(".galleryImage");
+// handle click on any img open gallery
+galleryImage.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    galleryConteiner.classList.add("active");
+  });
+});
+// handle open gallery from buton see more
 buttonGallery &&
   buttonGallery.addEventListener("click", () => {
     galleryConteiner.classList.add("active");
   });
+// handle close gallery from x button
 buttonCloseGallery &&
   buttonCloseGallery.addEventListener("click", () => {
     galleryConteiner.classList.remove("active");
   });
+// handle close gallery from anywhere on screen
+window.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element == galleryConteiner) {
+    galleryConteiner.classList.remove("active");
+  }
+});
+// add and remove active class from taps
 itineraryPackage &&
   itineraryPackage.forEach((item, index) => {
     item.addEventListener("click", () => {
